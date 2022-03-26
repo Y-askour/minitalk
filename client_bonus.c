57d20c8dc3be042563daa18326c7da9ceb11802c
Younes Askour <yaskour@e1r5p1.1337.ma>
2022-03-26 18:12:35 +0100

    last push inchallah

---
+++ client_bonus.c
@@ -0,0 +1,58 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   client.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yaskour <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/03/17 17:01:40 by yaskour           #+#    #+#             */
+/*   Updated: 2022/03/17 18:48:05 by yaskour          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#include <stdio.h>
+#include <signal.h>
+#include <unistd.h>
+#include "libft/libft.h"
+
+void handler(sig)
+{
+	if (sig == SIGUSR1)
+		ft_putstr_fd("mesage recieved\n",1);
+}
+
+void	send_msg(int pid, char *str)
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		j = 0;
+		while (j < 8)
+		{
+			if ((str[i] & 128 >> j) != 0)
+				kill(pid, SIGUSR1);
+			else
+				kill(pid, SIGUSR2);
+			usleep(800);
+			j++;
+		}
+		i++;
+	}
+}
+
+int	main(int argc, char **argv)
+{
+	int	pid;
+
+	if (argc == 3)
+	{
+		pid = atoi(argv[1]);
+		send_msg(pid, argv[2]);
+		signal(SIGUSR1,&handler);
+	}
+	else
+		ft_putstr_fd("invalid arguments\n", 2);
+	return (0);
+}
---
+++ server_bonus.c
@@ -0,0 +1,61 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   server.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yaskour <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/03/17 17:01:32 by yaskour           #+#    #+#             */
+/*   Updated: 2022/03/24 18:14:10 by yaskour          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#include <stdio.h>
+#include <unistd.h>
+#include <signal.h>
+#include <stdlib.h>
+#include "libft/libft.h"
+
+void	handle_sig(int sig, siginfo_t *info, void *test)
+{
+	static char		printed;
+	static int		shifter;
+	static pid_t	client_pid;
+
+	if (client_pid == 0 || info->si_pid != client_pid)
+	{
+		printf("\n pid of client %d\n",info->si_pid);
+		printed = 0b11111111;
+		shifter = 0;
+		client_pid = info->si_pid;
+		write(1,"\n",1);
+	}
+	if (sig == SIGUSR1)
+		printed = printed | 128 >> shifter;
+	else
+		printed = printed ^ 128 >> shifter;
+	shifter++;
+	if (shifter == 8)
+	{
+		write(1, &printed, 1);
+		shifter = 0;
+		printed = 0b11111111;
+	}
+}
+
+int	main(void)
+{
+	pid_t				pid;
+	struct sigaction	sa;
+
+	pid = getpid();
+	if (pid < 0)
+		return (0);
+	sa.sa_sigaction = &handle_sig;
+	sa.sa_flags = SA_SIGINFO;
+	ft_putnbr_fd(pid, 1);
+	sigaction(SIGUSR1, &sa, NULL);
+	sigaction(SIGUSR2, &sa, NULL);
+	while (1)
+		pause();
+	return (0);
+}
